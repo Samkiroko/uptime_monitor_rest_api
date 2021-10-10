a9aed7b288b8db1996bdf6de90b6ed58505e1e52
Samkiroko <kirokodev@gmail.com>
2021-10-10 21:09:38 +0300

    parsing payload through stream

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 // Dependencies
 const http = require('http')
 const url = require('url')
+const StringDecoder = require('string_decoder').StringDecoder
 
 //  The sever should respond to all request with a string
 const server = http.createServer(function (req, res) {
@@ -22,11 +23,19 @@ const server = http.createServer(function (req, res) {
 
   // Get the header as an object
   let headers = req.headers
-
-  // Send the response
-  res.end('Hello world\n')
-  // log  the request path
-  console.log('Request received with these header', headers)
+  // Get the payload, if any
+  let decoder = new StringDecoder('utf-8')
+  let buffer = ''
+  req.on('data', function (data) {
+    buffer += decoder.write(data)
+  })
+  req.on('end', function () {
+    buffer += decoder.end()
+    // Send the response
+    res.end('Hello world\n')
+    // log  the request path
+    console.log('Request received with these payload:', buffer)
+  })
 })
 // Start the server, and have it listen to port 3000
 server.listen(3000, function () {
